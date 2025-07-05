aaeb25b5a474782b763a63ed4eac0304a942a926
marko220991 <19marko91.ilic@gmail.com>
2025-07-05 16:01:49 +0200

    microservice (accounts): enhance docs of REST API using @OpenAPIDocumentation

--- accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybytes/accounts/AccountsApplication.java
@@ -1,11 +1,34 @@
 package com.eazybytes.accounts;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts Microservice REST API Documentation",
+				description = "Bank Accounts Microservice REST API Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "Marko Ilic",
+						email = "test@gmail.com"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://www.apache.org/licenses/LICENSE-2.0"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Bank Accounts Microservice REST API Documentation"
+		)
+)
 public class AccountsApplication {
 
 	public static void main(String[] args) {
