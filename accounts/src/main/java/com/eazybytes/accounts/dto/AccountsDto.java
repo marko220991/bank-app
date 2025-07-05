afb909e4b3c779d1b54486131c29e4e9a2188b43
marko220991 <19marko91.ilic@gmail.com>
2025-07-05 12:49:46 +0200

    microservice (accounts): preform input data validation

--- accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/AccountsDto.java
@@ -1,11 +1,18 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
 
+    @NotEmpty(message = "Account number cannot be empty or null")
+    @Pattern(regexp = "(^$|\\d{10})", message = "Account number must be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type cannot be empty or null")
     private String accountType;
+
+    @NotEmpty(message = "Branch address cannot be empty or null")
     private String branchAddress;
 }
--- accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
+++ accounts/src/main/java/com/eazybytes/accounts/dto/CustomerDto.java
@@ -1,12 +1,20 @@
 package com.eazybytes.accounts.dto;
 
+import jakarta.validation.constraints.*;
 import lombok.Data;
 
 @Data
 public class CustomerDto {
 
+    @NotEmpty(message = "Name cannot be empty or null")
+    @Size(min = 5, message = "Customer name should have at least 5 characters")
     private String name;
+
+    @Email(message = "Email should be valid")
+    @NotEmpty(message = "Email cannot be empty or null")
     private String email;
+
+    @Pattern(regexp = "(^$|\\d{10})", message = "Mobile number must be 10 digits")
     private String mobileNumber;
     private AccountsDto accountsDto;
 }
