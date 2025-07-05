a698590df09475ade2a42da9c154003a97ca3518
marko220991 <19marko91.ilic@gmail.com>
2025-07-05 13:04:35 +0200

    microservice (accounts): update audit columns using Spring Data

--- accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
+++ accounts/src/main/java/com/eazybytes/accounts/entity/BaseEntity.java
@@ -1,28 +1,36 @@
 package com.eazybytes.accounts.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.data.annotation.*;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
 public class BaseEntity {
 
+    @CreatedDate
     @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @CreatedBy
     @Column(updatable = false)
     private String createdBy;
 
+    @LastModifiedDate
     @Column(insertable  = false)
     private LocalDateTime updatedAt;
 
+    @LastModifiedBy
     @Column(insertable  = false)
     private String updatedBy;
 
